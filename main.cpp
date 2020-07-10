057f8626accad9893875b7f850f144f0cdb91c75
mlfcjob@163.com <mlfcjob@163.com>
2020-07-10 22:18:59 +0800

    add  command line parameters

--- main.cpp
+++ main.cpp
@@ -3,13 +3,17 @@
 
 using namespace std;
 
-int main(int argc, char* argv)
+int main(int argc, char* argv[])
 {
+	if (argc < 3)
+	{
+		cout << "usage: " << argv[0] << "[in_rtsp_url] [out_rtmp_url]" << endl;
+		cout << "eg: " << argv[0] << " rtsp://admin:admin@192.168.1.64 rtmp://192.168.1.102:1935/rtmp_live/test" << endl;
+		return -1;
+	}
 	RtspToRtmp* app = new RtspToRtmp();
-	std::string rtsp = "rtsp://admin:admin123@192.17.1.73";
-	rtsp = "rtsp://120.53.12.211/test1.sdp";
-	std::string rtmp = "rtmp://192.17.1.202:1935/rtmp_live/test";
-	rtmp = "rtmp://120.53.12.211:1935/rtmp_live/test";
+	std::string rtsp = argv[1];
+	std::string rtmp = argv[2];
 	app->Init(rtsp, rtmp);
 	app->Start();
 	getchar();
